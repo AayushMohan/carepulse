a5ee644f3efc83bdac30ffafba42fae1b5bd5d4b
AayushMohan <aayushmohan1702@gmail.com>
2024-07-17 14:49:55 +0530

    feat: Add react-phone-number-input dependency for capturing phone numbers in PatientForm
    This commit adds the "react-phone-number-input" dependency to the project's package.json file. This dependency will be used in the PatientForm component to capture and validate phone numbers. By including this dependency, we can ensure that users enter valid phone numbers and improve the overall user experience.

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -7,6 +7,10 @@ import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import CustomFormField from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
+import { useRouter } from "next/navigation";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -23,16 +27,34 @@ const formSchema = z.object({
     message: "Username must be at least 2 characters.",
   }),
 });
+
 const PatientForm = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+  async function onSubmit({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+
+    try {
+      // const userData = { name, email, phone };
+      // const user = await createUser(userData);
+      // if (user) router.push(`/patients/${user.id}/register`);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
@@ -53,7 +75,25 @@ const PatientForm = () => {
           iconAlt="user"
         />
 
-        <Button type="submit">Submit</Button>
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="johndoe@careplus.io"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="user"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone number"
+          placeholder="(123) 456-7890"
+        />
+
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
   );
